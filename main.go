85f91bc269f2f1fcb203529612d2b4f3fa981b47
Igor <dragmsc@gmail.com>
2022-05-24 08:38:54 +0300

    fix

--- main.go
+++ main.go
@@ -7,12 +7,9 @@ import (
 	"os"
 )
 
-var (
-	path string
-	l    int
-)
+var l int
 
-func read(dir string, firstSymbol string, level int) error {
+func tree(dir string, firstSymbol string, level int) error {
 	res, err := os.ReadDir(dir)
 	if err != nil {
 		return err
@@ -32,7 +29,7 @@ func read(dir string, firstSymbol string, level int) error {
 		}
 		if unit.IsDir() {
 			fmt.Printf("%s\033[1;35m%s\033[0m", firstSymbol+symbol, unit.Name()+"\n")
-			read(dir+"/"+unit.Name(), firstSymbol+nextSymbol, level+1)
+			tree(dir+"/"+unit.Name(), firstSymbol+nextSymbol, level+1)
 		} else {
 			fmt.Printf("%s\033[1;34m%s\033[0m", firstSymbol+symbol, unit.Name()+"\n")
 		}
@@ -41,15 +38,11 @@ func read(dir string, firstSymbol string, level int) error {
 }
 
 func main() {
+	var path string
 	flag.StringVar(&path, "path", ".", "path for tree")
 	flag.IntVar(&l, "level", -1, "deep of tree")
 	flag.Parse()
-	p, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("\033[1;35m%s\033[0m", p+"/"+path+"\n")
-	err = read(path, "", 0)
+	err := tree(path, "", 0)
 	if err != nil {
 		log.Fatal(err)
 	}
